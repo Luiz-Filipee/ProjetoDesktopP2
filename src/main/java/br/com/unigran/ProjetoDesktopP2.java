20da2b907b5ee800d906c2cc9b876c3ddfd1824c
Luiz-Filipee <luizkato7@gmail.com>
2024-05-01 16:43:11 -0400

    primeiro commit

---
+++ src/main/java/br/com/unigran/ProjetoDesktopP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.unigran;
+
+/**
+ *
+ * @author user
+ */
+public class ProjetoDesktopP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
