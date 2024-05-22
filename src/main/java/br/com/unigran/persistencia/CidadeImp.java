a4e46de77c60a67de7ec1fb6bb8b50bd424a13e3
Luiz-Filipee <luizkato7@gmail.com>
2024-05-22 18:16:22 -0400

    criando o dao da entidades funcao, endereco, cidade e estado

---
+++ src/main/java/br/com/unigran/persistencia/CidadeImp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.unigran.persistencia;
+
+import br.com.unigran.model.Cidade;
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public class CidadeImp extends CidadeDao{
+
+    @Override
+    public List<Cidade> listarCidades() {
+        return em.createNativeQuery("SELECT * FROM Cidade")
+                .getResultList();
+    }
+    
+}
